7377624bab6c6c2430d3db3bf420c35850093873
Magista08 <laip@rpi.edu>
2021-10-08 01:15:02 -0400

    Complete the basic function to get the row data

--- Problem1.py
+++ Problem1.py
@@ -4,15 +4,22 @@ import numpy as np
 OUTPUT = 0
 
 
-def GetData(FileName):
+def GetData(_FileName):
     # Test the file opened is correct or not
     ProcessedData = {
         'CO': [],
-        'PT08.S1': [],
-
+        'Tin Oxide': [],
+        'Hydro Carbons': [],
+        'Benzene': [],
+        'Titania':[],
+        'NOx': [],
+        'Tungsten Oxide3': [],
+        'NO2': [],
+        'Tungsten Oxide4': [],
+        'Indium Oxide': []
     }
     try:
-        OpenSignal = open(FileName)
+        OpenSignal = open(_FileName)
         OpenSignal.close()
     except FileNotFoundError:
         print("File is not found.")
@@ -24,16 +31,32 @@ def GetData(FileName):
     # Get the row data from the excel
     RowData = pd.read_excel('AirQualityUCI.xlsx')
 
+    # Define the basic parameter
+    LineLength = len(RowData.values[0])
+
+    # Add the Row Data into different sector
+    for i in RowData.values:
+        DataPtr = 2
+        for j in ProcessedData.keys():
+            ProcessedData[j].append(i[DataPtr])
+            DataPtr += 1
+            if DataPtr >= LineLength:
+                print("The excel's data: \n'{}'\n is out of range, Please check!".format(i))
+                exit(-1)
+
+    # Add the index num
+    Index = range(len(RowData.values))
+    ProcessedData['Index'] = Index
     '''
     # Test the row data is correct or not
     PrintData = RowData.values[0]
     print(PrintData)
     '''
 
-    return RowData
+    return ProcessedData
 
 
-def StochasticGD():
+def StochasticGD(_SequenceNum):
     return
 
 
@@ -41,3 +64,4 @@ if __name__ == "__main__":
     OriginalData = GetData("AirQualityUCI.xlsx")
 
     print("Data All ready")
+    print(OriginalData['CO'])
